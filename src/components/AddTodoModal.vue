47200df6da905b2a1fecb2fa5deba3217f8088bd
kimjihong9 <wlghddl9@gmail.com>
2023-01-21 14:36:24 +0900

    feat: [new todo modal] - organize input field

--- src/components/AddTodoModal.vue
+++ src/components/AddTodoModal.vue
@@ -6,14 +6,45 @@
           <div class="modal-header">
             <slot name="header"> default header </slot>
           </div>
-          <div class="modal-body">content</div>
+          <div class="modal-body">
+            <small class="section_title">Title</small>
+            <input v-model="input_title" class="title" type="text" />
+
+            <div class="input_section">
+              <div class="is-vertical">
+                <small class="section_title">Link</small>
+                <input v-model="input_link" class="link" type="text" />
+                <small class="section_title">Comment</small
+                ><textarea
+                  v-model="input_comment"
+                  class="comment"
+                  cols="50"
+                  rows="9"
+                ></textarea>
+              </div>
+              <div class="is-vertical">
+                <div id="app">
+                  <!-- v-date-picker -->
+                </div>
+                <div>
+                  <small class="section_title">co-doer</small
+                  ><input class="co-doer" type="text" />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </transition>
 </template>
-<script>
-export default {};
+<script setup>
+import { ref } from "vue";
+
+const input_title = ref("");
+const input_link = ref("");
+const input_comment = ref("");
+const coDoer_list = [];
 </script>
 <style>
 .modal-mask {
@@ -34,8 +65,8 @@ export default {};
 }
 
 .modal-container {
-  width: 70vh;
-  height: 50vh;
+  width: 50vh;
+  height: 30vh;
   margin: 0px auto;
   padding: 20px 30px;
   background-color: #fff;
@@ -43,5 +74,18 @@ export default {};
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
   transition: all 0.3s ease;
   color: black;
+}
+
+input {
+  display: block;
+  width: 300px;
+  height: 50px;
+  margin: 3px;
+}
+.input_section {
+  display: flex;
+}
+.section_title {
+  display: flex;
 }
 </style>
