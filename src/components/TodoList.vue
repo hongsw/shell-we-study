848f4e5b662edc3f76e3281b7f2bbaed8b639d66
kimjihong9 <wlghddl9@gmail.com>
2023-01-24 06:22:37 +0900

    feat: due sortable

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -8,7 +8,7 @@
     :defaultColDef="gridOptions.defaultColDef"
     :gridOptions="gridOptions"
     @grid-ready="onGridReady"
-    @cell-clicked="onCellDelete"
+    @cell-clicked="clickRowDelete"
   >
   </ag-grid-vue>
 
@@ -23,7 +23,6 @@
 <script setup>
 import AddTodoModal from "./AddTodoModal.vue";
 import { ref } from "vue";
-
 import "ag-grid-community/styles//ag-grid.css";
 import "ag-grid-community/styles//ag-theme-alpine.css";
 import { AgGridVue } from "ag-grid-vue3";
@@ -39,7 +38,7 @@ const myRowData = [
     title: "TOEIC",
     link: "https://www.toeic.co.kr/",
     coDoers: "jihong, ji, hong",
-    due: "Jan/30/2023",
+    due: "2023-01-30",
     understanding: "Not yet",
     del_btn: "❌",
   },
@@ -47,7 +46,7 @@ const myRowData = [
     title: "TOEIC",
     link: "https://www.toeic.co.kr/",
     coDoers: "jihong, ji, hong",
-    due: "Jan/31/2023",
+    due: "2023-01-31",
     understanding: "Not yet",
     del_btn: "❌",
   },
@@ -56,7 +55,7 @@ const myColumnDefs = [
   { headerName: "Title", field: "title", suppressMenu: true },
   { headerName: "Link", field: "link", suppressMenu: true },
   { headerName: "Co-doer", field: "coDoers", suppressMenu: true },
-  { headerName: "Due", field: "due", suppressMenu: true },
+  { headerName: "Due", field: "due", suppressMenu: true, sortable: true },
   {
     headerName: "Understanding",
     field: "understanding",
@@ -68,7 +67,7 @@ const myColumnDefs = [
     },
     suppressMenu: true,
   },
-  { headerName: "", field: "del_btn" },
+  { headerName: "", field: "del_btn", suppressMenu: true },
 ];
 
 const gridApi = null;
@@ -91,16 +90,64 @@ const clickAddTodo = () => {
   showModal.value = !showModal.value;
 };
 
+const convertMonthData = (month) => {
+  switch (month) {
+    case "Jan":
+      month = "01";
+      break;
+    case "Feb":
+      month = "02";
+      break;
+    case "Mar":
+      month = "03";
+      break;
+    case "Apr":
+      month = "04";
+      break;
+    case "May":
+      month = "05";
+      break;
+    case "Jun":
+      month = "06";
+      break;
+    case "Jul":
+      month = "07";
+      break;
+    case "Aug":
+      month = "08";
+      break;
+    case "Sep":
+      month = "09";
+      break;
+    case "Oct":
+      month = "10";
+      break;
+    case "Nov":
+      month = "11";
+      break;
+    case "Dec":
+      month = "12";
+      break;
+    default:
+      break;
+  }
+  return month;
+};
+
 const addNewTodo = (input_data) => {
   showModal.value = false;
 
   if (input_data.title.value != "") {
-    const date_list = input_data.due.value.toString().split(" ").slice(1, 4);
+    const date_list = input_data.due.value.toString().split(" ");
+    date_list[1] = convertMonthData(date_list[1]);
+    date_list[2] = date_list[2].padStart(2, "0");
+
+    const date_info = [date_list[3], date_list[1], date_list[2]];
     const tmp = {
       title: input_data.title.value,
       link: input_data.link.value,
       comment: input_data.link.value,
-      due: date_list.join("/"),
+      due: date_info.join("-"),
       coDoers: input_data.coDoers.join(),
       understanding: "Not yet",
       del_btn: "❌",
@@ -112,15 +159,15 @@ const addNewTodo = (input_data) => {
 
 const clickRemoveTodo = () => {
   const selectedRow = gridOptions.api.getFocusedCell();
-  console.log(selectedRow);
   myRowData.splice(selectedRow.rowIndex, 1);
   gridOptions.api.setRowData(myRowData);
 };
 
-const onCellDelete = (params) => {
+const clickRowDelete = (params) => {
   const col_name = params.column.colId;
   if (col_name === "del_btn") {
     const selectedRow = gridOptions.api.getFocusedCell();
+    console.log(selectedRow);
     myRowData.splice(selectedRow.rowIndex, 1);
     gridOptions.api.setRowData(myRowData);
   }
